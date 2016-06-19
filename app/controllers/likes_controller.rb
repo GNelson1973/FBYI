40382680ee0e032c319bc5cd63664edd8793e6bb
GNelson1973 <jorge.oosthoek@gmail.com>
2016-06-19 20:40:43 +0200

    authorize controller actions

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,9 +3,11 @@ def create
   post = Post.find( params[:post_id] )
 
   if like = Like.find_by( post: post, user: current_user )
+    authorize! :destroy, like
     like.destroy
   else
     like = Like.new( post: post, user: current_user )
+    authorize! :create, like
     like.save
   end
 
