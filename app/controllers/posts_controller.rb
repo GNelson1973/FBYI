1826209e8789e030ad50fb351e46116daffb274a
GNelson1973 <jorge.oosthoek@gmail.com>
2016-06-19 20:30:33 +0200

    user likes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,6 +19,7 @@ end
 def user
   @user = User.find( params[:user_id] )
   @posts = Post.where( user: @user ).order( created_at: :desc )
+  @likes = @user.likes.joins( :post ).order( "posts.created_at DESC" )
 end
 
 end
