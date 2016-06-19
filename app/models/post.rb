fd253b418c303eda37dbfde799490c7df4f6f10a
GNelson1973 <jorge.oosthoek@gmail.com>
2016-06-19 19:58:01 +0200

    like model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,4 @@
 class Post < ActiveRecord::Base
   belongs_to :user
-
-def create
-  # ...
-  @post = Post.new( content: post_params[:content] )
-  @post.user = current_user
-  # ...
-end
-
+  has_many :likes
 end
