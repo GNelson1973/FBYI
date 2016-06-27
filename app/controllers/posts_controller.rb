ce97316566cbe15b066ed863b41686649aee2f6c
GNelson1973 <jorge.oosthoek@gmail.com>
2016-06-27 22:32:47 +0200

    devise views users

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,32 +3,26 @@ class PostsController < ApplicationController
 
   def index
     @posts = @posts.order( created_at: :desc )
-    authorize! :read, @posts
   end
 
-def create
-     post_params = params.require( :post ).permit( :content )
-
-     @post = Post.new( content: post_params[:content] )
-     @post.user = current_user
-     authorize! :create, @post
-     if @post.save
+  def create
+      if @post.save
         redirect_to posts_path
-     else
+      else
         render posts_path
-     end
+      end
   end
 
-def user
   def user
-     @user = User.find( params[:user_id] )
-     authorize! :read, @user
-     @posts = Post.where( user: @user ).order( created_at: :desc )
-     authorize! :read, @posts
-     @likes = @user.likes.joins( :post ).order( "posts.created_at DESC" )
-     authorize! :read, @likes
+    def user
+       @user = User.find( params[:user_id] )
+       authorize! :read, @user
+       @posts = Post.where( user: @user ).order( created_at: :desc )
+       authorize! :read, @posts
+       @likes = @user.likes.joins( :post ).order( "posts.created_at DESC" )
+       authorize! :read, @likes
+    end
   end
-end
 
 private
   def post_params
